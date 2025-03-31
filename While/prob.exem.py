70d413bdce0e51f5d09fb6fa2ccbbed15b4eb256
Nicole Caroline <nicolemegvieira@gmail.com>
2025-03-31 13:45:07 -0300

    while comando break

--- While/prob.exem.py
+++ While/prob.exem.py
@@ -3,3 +3,9 @@ while x > 0:
     print("Loop infinito!")
     x = x - 1
     #Falta decrementar x
+
+
+contador = 5
+while contador > 0:
+    print("Infinit!")
+    contador -= 1
---
+++ While/whileTrue.py
@@ -0,0 +1,13 @@
+numero = 0
+while True: #Loop Infinito
+    numero = numero + 1
+    print(numero)
+    if numero == 5:
+        break  # Sai de loop
+
+numero = 0
+while True: #Loop Infinito
+    numero += 1
+    print(numero)
+    if numero == 5:
+        break  # Sai de loop
