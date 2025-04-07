386028cdf689ee8314aad9435283968d3ab5fc5b
Nicole Caroline <nicolemegvieira@gmail.com>
2025-04-07 10:31:43 -0300

    Criar atividade filmes for lista

---
+++ Vetores (Listas)/ativ.pratic.3.py
@@ -0,0 +1,6 @@
+import time
+
+filmes = ["ele é demais", "culpa mia", "atraves da minha janela", "para todos os garotos que ja amei"]
+print("todos meus filmes favoritos")
+for filme in filmes:
+    time.sleep(0.5),print(filme)
