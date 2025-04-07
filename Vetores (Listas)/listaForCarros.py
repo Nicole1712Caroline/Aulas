b128582b14798602e9147de04d2cf8ee849e0159
Nicole Caroline <nicolemegvieira@gmail.com>
2025-04-07 10:17:08 -0300

    Criar Lista FOR carros

---
+++ Vetores (Listas)/listaForCarros.py
@@ -0,0 +1,6 @@
+#Criando uma lista de carros
+modelosCarros = ["fusca", "gol", "chevete", "bmw", "mustang"]
+
+print("Carros em minha lista: ")
+for i, itens in enumerate(modelosCarros):
+    print(i, itens)
