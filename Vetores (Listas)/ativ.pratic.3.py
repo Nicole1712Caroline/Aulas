21bcd6cee63bec1ae72e776fc4b4b8c00cbace85
Nicole Caroline <nicolemegvieira@gmail.com>
2025-04-07 10:37:43 -0300

    Criar lista presetes total em números

--- Vetores (Listas)/ativ.pratic.3.py
+++ Vetores (Listas)/ativ.pratic.3.py
@@ -3,4 +3,4 @@ import time
 filmes = ["ele é demais", "culpa mia", "atraves da minha janela", "para todos os garotos que ja amei"]
 print("todos meus filmes favoritos")
 for filme in filmes:
-    time.sleep(0.5),print(filme)
+    print(filme)
---
+++ Vetores (Listas)/presentesLista.py
@@ -0,0 +1,9 @@
+#Criando uma lista de números
+numeros = [5, 10, 15, 20, 25, 30]
+
+#Descobrindo o tamanho da lista
+tamanho_da_lista = len(numeros)
+
+#Exibindo o resultado
+print(f"O tamanho da lista é: {tamanho_da_lista}")
+#Saída o tamanho da lista é: 6
