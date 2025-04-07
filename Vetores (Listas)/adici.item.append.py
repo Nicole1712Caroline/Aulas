c5adb1d27ba1a6c4b9387917f110ca0cfa41d939
Nicole Caroline <nicolemegvieira@gmail.com>
2025-04-07 09:42:01 -0300

    Criar adicionar item append

---
+++ Vetores (Listas)/adici.item.append.py
@@ -0,0 +1,12 @@
+#METODO APPEND()
+#adiciona um item ao final da lista.
+animais = []
+
+animais = ["cachorro", "gato"]
+animais.append("pássaro")
+#Resultado: ["cachorro", "gato", "pássaro"]
+
+#METODO INSERT()
+#adiciona um item em um ídice específico.
+animais = ["cachorro", "gato"]
+animais.insert(1, "coelho")
